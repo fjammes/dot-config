887283af2e36c3174eab7f65bdee53fa28d8c68f
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2017-09-07 11:21:17 +0200

    fix rm

--- configure.sh
+++ configure.sh
@@ -75,7 +75,7 @@ sed "s/\" colorscheme solarized/colorscheme solarized/" $HOME/.vimrc
 $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer --gocode-complete
 
 # zsh
-rm -r ~/.oh-my-zsh ~/.zshrc
+rm -rf ~/.oh-my-zsh ~/.zshrc
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 sudo -s -- /bin/chsh -s /bin/zsh qserv
